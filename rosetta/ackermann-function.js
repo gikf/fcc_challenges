80f12c717cc4e7c779591fcfa2495fc615fb9722
sanityto <60067306+sanityto@users.noreply.github.com>
2020-05-30 06:09:42 +0200

    added solution for ackermann function

---
+++ rosetta/ackermann-function.js
@@ -0,0 +1,9 @@
+function ack(m, n) {
+  if (m == 0) {
+    return n + 1;
+  } else if (m > 0 && n == 0) {
+    return ack(m - 1, 1);
+  } else if (m > 0 && n > 0) {
+    return ack(m - 1, ack(m, n - 1));
+  }
+}
