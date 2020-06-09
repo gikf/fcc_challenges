83a27732fb3f7626012d27dba8b42d0389ec32aa
sanityto <60067306+sanityto@users.noreply.github.com>
2020-06-09 13:29:15 +0200

    added solution for use depth first seach in a binary search tree

---
+++ data_structures/32_use-depth-first-search-in-a-binary-search-tree.js
@@ -0,0 +1,71 @@
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+function BinarySearchTree() {
+  this.root = null;
+
+  // change code below this line
+  // In-order DFS
+  this.inorder = () => {
+    if (this.root === null) {
+      return null;
+    }
+
+    function traverse(tree) {
+      if (tree.left === null && tree.right === null) {
+        return [tree.value];
+      }
+
+      const leftTraverse = traverse(tree.left);
+      const rightTraverse = traverse(tree.right);
+
+      return leftTraverse.concat(tree.value).concat(rightTraverse);
+    }
+
+    return traverse(this.root);
+  }
+
+  // Pre-order DFS
+  this.preorder = () => {
+    if (this.root === null) {
+      return null;
+    }
+
+    function traverse(tree) {
+      if (tree.left === null && tree.right === null) {
+        return [tree.value];
+      }
+
+      const leftTraverse = traverse(tree.left);
+      const rightTraverse = traverse(tree.right);
+
+      return [tree.value].concat(leftTraverse, rightTraverse);
+    }
+
+    return traverse(this.root);
+  }
+
+  // Post-order DFS
+  this.postorder = () => {
+    if (this.root === null) {
+      return null;
+    }
+
+    function traverse(tree) {
+      if (tree.left === null && tree.right === null) {
+        return [tree.value];
+      }
+
+      const leftTraverse = traverse(tree.left);
+      const rightTraverse = traverse(tree.right);
+
+      return leftTraverse.concat(rightTraverse, tree.value);
+    }
+
+    return traverse(this.root);
+  }
+  // change code above this line
+}
