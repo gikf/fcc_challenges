744db5180e3b119af2a347556170560c2b0e4642
sanityto <60067306+sanityto@users.noreply.github.com>
2020-06-22 22:23:13 +0200

    fix wrong reordering heap after removing elements

--- data_structures/40_remove-an-element-from-a-max-heap.js
+++ data_structures/40_remove-an-element-from-a-max-heap.js
@@ -18,22 +18,34 @@ var MaxHeap = function() {
       return null;
     }
     const maxValue = this.heap[1];
-    this.heap[1] = this.heap.pop();
+    if (this.heap.length > 2) {
+      this.heap[1] = this.heap.pop();
+    } else {
+      this.heap.pop();
+    }
 
-    for (let i = 2; i < this.heap.length; i ++) {
-      let curParent = Math.floor(i / 2);
+    let curParent = 1;
+
+    while (curParent < this.heap.length) {
       let curChild1 = curParent * 2;
       let curChild2 = curParent * 2 + 1;
 
-      if (this.heap[curParent] > this.heap[curChild1]
-          && this.heap[curParent] > this.heap[curChild2]) {
-            break;
-          }
+      const child1_compare = this.heap[curParent] < this.heap[curChild1];
+      const child1_compare_or_nan = child1_compare || isNaN(this.heap[curChild1]);
+      
+      const child2_compare = this.heap[curParent] < this.heap[curChild2];
+      const child2_compare_or_nan = child2_compare || isNaN(this.heap[curChild2]);
 
-      if (this.heap[curChild1] > this.heap[curChild2]) {
+      if (child1_compare_or_nan && child2_compare_or_nan) {
+        break;
+      }
+
+      if (this.heap[curChild1] > this.heap[curChild2] || isNaN(this.heap[curChild2])) {
         [this.heap[curParent], this.heap[curChild1]] = [this.heap[curChild1], this.heap[curParent]];
-      } else {
+        curParent = curChild1;
+      } else if (!isNaN(this.heap[curChild2])) {
         [this.heap[curParent], this.heap[curChild2]] = [this.heap[curChild2], this.heap[curParent]];
+        curParent = curChild2;
       }
     }
 
