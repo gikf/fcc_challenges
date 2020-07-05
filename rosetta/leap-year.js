816d8f709b9b2fe9fa2834316eef26761dd0a6d3
sanityto <60067306+sanityto@users.noreply.github.com>
2020-07-05 05:58:20 +0200

    added solution for leap year

---
+++ rosetta/leap-year.js
@@ -0,0 +1,11 @@
+function isLeapYear(year) {
+  if (year % 4 != 0) { 
+    return false;
+  } else if (year % 100 != 0) {
+    return true;
+  } else if (year % 400 != 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
