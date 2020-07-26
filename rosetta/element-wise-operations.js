e906d6740b6863ae627aeb8359ea6cb3212e3c03
sanityto <60067306+sanityto@users.noreply.github.com>
2020-07-26 09:35:29 +0200

    Add solution for Element-wise operations.

---
+++ rosetta/element-wise-operations.js
@@ -0,0 +1,42 @@
+function operation(op, arr1, arr2) {
+  function matrix_action(action, result) {
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr1[0].length; j++) {
+        result[i][j] = action(arr1[i][j], arr2[i][j]);
+      }
+    }
+  }
+  function scalar_action(action, scalar, result) {
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr1[0].length; j++) {
+        result[i][j] = action(arr1[i][j], scalar)
+      }
+    }
+  }
+  const result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    result.push([]);
+    for (let j = 0; j < arr1[0].length; j++) {
+      result[i].push(0);
+    }
+  }
+
+  const actions = {
+    'add': (a, b) => a + b,
+    'sub': (a, b) => a - b,
+    'mult': (a, b) => a * b,
+    'div': (a, b) => a / b,
+    'exp': (a, b) => a**b,
+  }
+
+  const scalar = 2;
+  const [operation, action] = op.split('_');
+
+  if (operation == 's') {
+    scalar_action(actions[action], scalar, result)
+  } else if (operation == 'm') {
+    matrix_action(actions[action], result)
+  }
+
+  return result;
+}
